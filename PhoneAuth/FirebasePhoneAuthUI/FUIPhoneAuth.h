73603f2f8382885da7d7361c7c20920e2643d199
Christian Niles <cniles@twitter.com>
2019-10-03 18:40:59 -0700

    Cleanup imports for modules

--- PhoneAuth/FirebasePhoneAuthUI/FUIPhoneAuth.h
+++ PhoneAuth/FirebasePhoneAuthUI/FUIPhoneAuth.h
@@ -14,7 +14,7 @@
 //  limitations under the License.
 //
 
-#import "FirebaseAuthUI.h"
+#import <FirebaseAuthUI/FirebaseAuthUI.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
--- PhoneAuth/FirebasePhoneAuthUI/FUIPhoneEntryViewController.h
+++ PhoneAuth/FirebasePhoneAuthUI/FUIPhoneEntryViewController.h
@@ -14,7 +14,7 @@
 //  limitations under the License.
 //
 
-#import "FirebaseAuthUI.h"
+#import <FirebaseAuthUI/FirebaseAuthUI.h>
 
 @class FUICountryCodes;
 
